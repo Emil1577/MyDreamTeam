1de5f11a20c991d4bdb236f81a55cf7f44564fd5
Emil1577 <emilronquillo@gmail.com>
2023-02-02 20:08:49 -0800

    adding the Q&A portion with functions

--- server.js
+++ server.js
@@ -23,48 +23,142 @@ const db = mysql.createConnection(
     console.log(`Connected to the mydreamteam_db database.`)
 );
 
+const initialQuestions = [
 
-
-
-
-db.query('SELECT * FROM department;', function (err, data) {
-    console.table(data);
-
-    console.log(data[0].department_name);
-// using for loop to get the values on the department and make it as a list on the questions
-    var departmentList = [];
-
-    for (let i = 0; i < data.length; i++) {
-        departmentList.push(data[i].department_name);
+    {
+        name: 'selection',
+        type: 'list',
+        message: 'Please choose a license',
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "EXIT"
+        ]
     }
+]
 
-    console.log(departmentList);
+function initializeAnswers() {
+   
+    // prompt user actions using inquirer 
+    inquirer.prompt(initialQuestions)
+    
+    // await user responce from inquirer
+    .then(function(initialQuestions) {
 
-    const questions = [
+        if(initialQuestions.selection == "View All Employees") {         
 
-        {
-            type: 'list',
-            message: 'Please choose a license',
-            name: 'license',
-            choices: departmentList,
-        }
-    ]
+            viewAllEmployees();
+        
+        }else if(initialQuestions.selection  == "View All Departments") {
 
-    // Prompt function for question and answer
-    inquirer
-        .prompt(questions)
+            viewAllDepartments();
 
-        //generating data based on the answers/response
-        .then((response) => {
+        }else if(initialQuestions.selection  == "View All Roles") {
 
-            console.log(response);
+            viewAllRoles();
 
-        })
+        }else if(initialQuestions.selection  == "Add employee") {
 
-});
+            addEmployee();
+            
+        }else if(initialQuestions.selection  == "Add department") {
+
+            addDept();
+       
+        }else if(initialQuestions.selection  == "Add role") {
+
+            addRole();
+
+        }else if(initialQuestions.selection  == "Edit employee") {
+
+            updateEmployee();
+
+        }else if(initialQuestions.selection  == "Remove employee") {
+
+            deleteEmployee();
+
+        }else if(initialQuestions.selection  == "EXIT") {
+
+            exit();
+
+        };
+        
+
+    });    
+
+};
 
 
+function viewAllEmployees(){
+    console.log("triggered")
 
+    db.query('SELECT * FROM employees;', function (err, data) {
+        console.table(data);
+        initializeAnswers()
+    });
+    
+}
+
+function viewAllRoles(){
+
+    db.query('SELECT * FROM roles;', function (err, data) {
+        console.table(data);
+    });
+    
+}
+
+function viewAllDepartments(){
+
+    db.query('SELECT * FROM roles;', function (err, data) {
+        console.table(data);
+    });
+    
+}
+
+
+// db.query('SELECT * FROM department;', function (err, data) {
+//     console.table(data);
+
+//     console.log(data[0].department_name);
+//     // using for loop to get the values on the department and make it as a list on the questions
+//     var departmentList = [];
+
+//     for (let i = 0; i < data.length; i++) {
+//         departmentList.push(data[i].department_name);
+//     }
+
+//     console.log(departmentList);
+
+//     const questions = [
+
+//         {
+//             type: 'list',
+//             message: 'Please choose a license',
+//             name: 'license',
+//             choices: departmentList,
+//         }
+//     ]
+
+//     // Prompt function for question and answer
+//     inquirer
+//         .prompt(questions)
+
+//         //generating data based on the answers/response
+//         .then((response) => {
+
+//             console.log(response);
+
+//         })
+
+// });
+
+
+initializeAnswers();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
