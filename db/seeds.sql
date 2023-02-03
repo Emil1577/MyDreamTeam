e03a02b587760a1cf545e57efcae86d8c64b3ef6
Emil1577 <emilronquillo@gmail.com>
2023-02-02 19:29:19 -0800

    got the multiple choise to work

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,3 @@
-INSERT INTO employees (first_name, last_name)
-VALUES ("Arthur", "Miller"),
-       ("Chinua", "Achebe"),
-       ("Margaret", "Atwood"),
-       ("Gabriel", "Garcia Marquez"),
-       ("Simone", "de Beauvoir");
-       
-INSERT INTO roles (title, salary)
-VALUES ("sales", 100000),
-       ("customer service", 70000),
-       ("engineer", 150000),
-       ("lawyer", 210000);
-
 INSERT INTO department (department_name)
 VALUES ("Sales"),
        ("Engineering"),
@@ -18,7 +5,20 @@ VALUES ("Sales"),
         ("Service"),
        ("Law Group");
 
-       INSERT INTO manager (manager_name)
+INSERT INTO manager (manager_name)
 VALUES ("Joe Doe"),
        ("Jane Bane"),
-       ("Rick Shaw"),
+       ("Rick Shaw");       
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("sales", 100000, 1),
+       ("customer service", 70000, 4),
+       ("engineer", 150000, 2),
+       ("lawyer", 210000, 5);
+       
+INSERT INTO employees (first_name, last_name, manager_id, roles_id)
+VALUES ("Arthur", "Miller", 1, 1),
+       ("Chinua", "Achebe", 2, 3),
+       ("Margaret", "Atwood", 2, 4),
+       ("Gabriel", "Garcia Marquez", 1, 2),
+       ("Simone", "de Beauvoir", 3, 4);
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const inquirer = require('inquirer')
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'mydreamteam_db'
+  },
+  console.log(`Connected to the mydreamteam_db database.`)
+);
+
+
+db.query('SELECT * FROM department;' , function (err, data){
+        console.table(data);
+
+        console.log(data[0].department_name);
+
+         var departmentList=[];
+
+        for (let i=0; i<data.length; i++){
+
+     
+
+            departmentList.push(data[i].department_name);
+
+        }
+
+console.log(departmentList);
+
+const questions =[
+
+    
+    {
+        type: 'list',
+        message: 'Please choose a license',
+        name: 'license',
+        choices: departmentList,
+    }
+]
+
+// Prompt function for question and answer
+inquirer
+    .prompt(questions)
+    
+    //generating data based on the answers/response
+    .then((response) => {
+         
+
+        console.log(response);
+
+
+    })
+
+    });
+    
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
