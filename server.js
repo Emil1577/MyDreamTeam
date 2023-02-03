f53b353fead7f5653ca933583572a61bd3bc47cd
Emil1577 <emilronquillo@gmail.com>
2023-02-02 19:49:03 -0800

    added multiple choice formula

--- server.js
+++ server.js
@@ -12,62 +12,60 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: 'password',
-    database: 'mydreamteam_db'
-  },
-  console.log(`Connected to the mydreamteam_db database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'password',
+        database: 'mydreamteam_db'
+    },
+    console.log(`Connected to the mydreamteam_db database.`)
 );
 
 
-db.query('SELECT * FROM department;' , function (err, data){
-        console.table(data);
 
-        console.log(data[0].department_name);
 
-         var departmentList=[];
 
-        for (let i=0; i<data.length; i++){
+db.query('SELECT * FROM department;', function (err, data) {
+    console.table(data);
 
-     
+    console.log(data[0].department_name);
+// using for loop to get the values on the department and make it as a list on the questions
+    var departmentList = [];
 
-            departmentList.push(data[i].department_name);
-
-        }
-
-console.log(departmentList);
-
-const questions =[
-
-    
-    {
-        type: 'list',
-        message: 'Please choose a license',
-        name: 'license',
-        choices: departmentList,
+    for (let i = 0; i < data.length; i++) {
+        departmentList.push(data[i].department_name);
     }
-]
 
-// Prompt function for question and answer
-inquirer
-    .prompt(questions)
-    
-    //generating data based on the answers/response
-    .then((response) => {
-         
+    console.log(departmentList);
 
-        console.log(response);
+    const questions = [
+
+        {
+            type: 'list',
+            message: 'Please choose a license',
+            name: 'license',
+            choices: departmentList,
+        }
+    ]
+
+    // Prompt function for question and answer
+    inquirer
+        .prompt(questions)
+
+        //generating data based on the answers/response
+        .then((response) => {
+
+            console.log(response);
+
+        })
+
+});
 
 
-    })
 
-    });
-    
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
